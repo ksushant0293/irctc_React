7cd329bf927faaa26e51d1e282360e13c5c9408b
sushant kumar <ksushant0293@gmail.com>
2024-07-29 17:55:39 +0530

    dsf

--- src/Finalbooking/Finalbooking.jsx
+++ src/Finalbooking/Finalbooking.jsx
@@ -3,18 +3,18 @@ import Header from "../components/header/Header";
 import "./Finalbooking.css"
 import arrow from "../assets/arrow.png"
 
-const trainName = localStorage.getItem("trainName");
-console.log(trainName);
-const toStation = localStorage.getItem("toStation");
-console.log();
-const fromStation = localStorage.getItem("fromStation");
-const travelDate = localStorage.getItem("travelDate");
-console.log(travelDate);
-const fromStationName = localStorage.getItem("fromStationName");
-console.log(fromStationName);
-const toStationName = localStorage.getItem("toStationName");
-console.log(toStationName);
-const Pnr = Math.floor(Math.random() * 10000000);
+// const trainName = localStorage.getItem("trainName");
+// console.log(trainName);
+// const toStation = localStorage.getItem("toStation");
+// console.log();
+// const fromStation = localStorage.getItem("fromStation");
+// const travelDate = localStorage.getItem("travelDate");
+// console.log(travelDate);
+// const fromStationName = localStorage.getItem("fromStationName");
+// console.log(fromStationName);
+// const toStationName = localStorage.getItem("toStationName");
+// console.log(toStationName);
+// const Pnr = Math.floor(Math.random() * 10000000);
 // const data = localStorage.getItem("passengerFormData")
 // const passengerName = JSON.parse(data);
 // console.log(passengerName.name);
@@ -24,6 +24,19 @@ const Pnr = Math.floor(Math.random() * 10000000);
 // const contact = localStorage.getItem("passengerFormData") ? (JSON.parse(localStorage.getItem("passengerFormData"))).contact : "999999999";
 
 const Finalbooking = () => {
+  
+  const trainName = localStorage.getItem("trainName");
+  console.log(trainName);
+  const toStation = localStorage.getItem("toStation");
+  console.log();
+  const fromStation = localStorage.getItem("fromStation");
+  const travelDate = localStorage.getItem("travelDate");
+  console.log(travelDate);
+  const fromStationName = localStorage.getItem("fromStationName");
+  console.log(fromStationName);
+  const toStationName = localStorage.getItem("toStationName");
+  console.log(toStationName);
+  const Pnr = Math.floor(Math.random() * 10000000);
   const name = (JSON.parse(localStorage.getItem("passengerFormData"))).name;
   const email = (JSON.parse(localStorage.getItem("passengerFormData"))).email;
   const contact = (JSON.parse(localStorage.getItem("passengerFormData"))).contact;
@@ -64,9 +77,9 @@ const Finalbooking = () => {
         </div>
         <div className="bookedPassengerDetails">
           <h2>Passenger Details</h2>
-          {/* <h4>name- {name}</h4> */}
-          {/* <h4>email- {email}</h4> */}
-          {/* <h4>mobile- {contact}</h4> */}
+          <h4>name- {name}</h4>
+          <h4>email- {email}</h4>
+          <h4>mobile- {contact}</h4>
         </div>
         <div className="date">
           <h2>your seat is confirmed! happy journey</h2>
